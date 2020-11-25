3c457319b6ea35526ccd7d7c761f2fc2066cc0e6
akitasummer <644171127@qq.com>
2020-11-25 22:03:39 +0800

    test: commit url: https://123.com

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const getCommandRetVal = command => {
     return execSync(command).toString('utf8').trim();
 };
 
-const commitMsgs = getCommandRetVal('git log').split('\n');
+const commitMsgs = getCommandRetVal('git log').split('commit').map(msg => {
+    if (!msg) return
+    const msgArr = msg.split('\n')
+    const hash = msgArr[0].trim()
+    const author = msgArr[1].replace('Author:', '').trim()
+    const date = msgArr[2].replace('Date:', '').trim()
+    const content = msgArr.splice(4).reduce((prev))
+    if (/^(doc:)/.test(msgArr[4].trim())) {}
+    console.log(/^(doc)/.test(msgArr[4].trim()))
+});
 
 console.log(commitMsgs)
